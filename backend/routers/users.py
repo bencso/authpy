0ea3fad0d5bc6d6dc82d001c2cad21c47de15df6
Bencso <158219939+bencso@users.noreply.github.com>
2026-01-10 20:57:34 +0100

    Fix token URL in OAuth2PasswordBearer and clean up check-token function

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -7,7 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 import bcrypt
 
 templates = Jinja2Templates(directory="html_templates")
-ouauth_security = OAuth2PasswordBearer(tokenUrl="users/check-token")
+ouauth_security = OAuth2PasswordBearer(tokenUrl="api/v1/users/check-token")
 salt = bcrypt.gensalt()
 
 router = APIRouter(
@@ -19,7 +19,6 @@ router = APIRouter(
 @router.post("/create-temporary-user", summary="Temporary QR kód létrehozás", description="Az admin felhasználó, létre tud hozni egy temporary qr kódot, amit ha beolvas majd a felhasználó, az átírányítja őt egy forms oldalra, ahol tud regisztrálni")
 async def create_temporary_qr(request: Request,token: Annotated[str, Depends(ouauth_security)]):
     random_uid=uuid.uuid4()
-    print(token)
     #TODO: Admin felhasználó check, az után pedig majd még ide kell, 
     # egy olyan hogy ez a random uuid-t felteszem db-be és frontenden csinálni neki egy felületet forms-nak és oda irányít majd a qrcode
     qrcode = segno.make("http://localhost:8001/temporary-qr?q="+str(random_uid))
@@ -29,18 +28,19 @@ async def create_temporary_qr(request: Request,token: Annotated[str, Depends(oua
     
 @router.post("/check-token", summary="Token check")
 async def check_token(request: Request):
-    body = await request.body()
     try:
+        body = await request.body()
+        if(body is None):
+            pass
         stringed_body = body.decode("utf-8")
         [_, b_uname, b_pass] = stringed_body.split('&')
-        print(b_uname)
-        if(len(b_uname)<= 0 or len(b_pass)<=0):
-            pass
         username = b_uname.split("=").pop(-1)
         password = b_pass.split("=").pop(-1).encode("utf-8")
+        if(len(username)<= 0 or len(password)<=0):
+            pass
         hashed_password = bcrypt.hashpw(password,salt)
         #TODO: Majd itt checkolni hogy tényleg létezik e ilyen user :) és ezalapján visszaadni
         return "sikeres"
     except:
-        print("HIBA!")
+        print("HIBA, a checkolás során!")
         return False
